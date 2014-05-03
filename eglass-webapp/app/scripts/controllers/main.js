840d9ccdf9e1e6fce00a965d13aba49d12633d9a
Joarley Santos <joarley.santos@gmail.com>
2014-05-03 15:08:30 -0300

    Commit inicial do sistema

---
+++ eglass-webapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('eglassWebappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
