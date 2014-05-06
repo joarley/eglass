f7314fbeac9aef685375be80eb6aab9456e90037
rudrigodefreitasoliveira <rodrigodefreitasoliveira@gmail.com>
2014-05-06 00:38:33 -0300

    a,saksj

---
+++ eglass-serverapp/Rajastech.EGlass.Infrastructure.Data.EntityFramework.Core/UnitOfWork/IEntityFrameworkUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Runtime.InteropServices;
+using Reply.Brazil.WCMTools.Domain.Core;
+
+namespace Reply.Brazil.WCMTools.Infrastructure.Persistence.Data.Core.UnitOfWork
+{
+    public interface IEntityFrameworkUnitOfWork<TEntity> : IUnitOfWork where TEntity: class, IEntity
+    {
+        IDbSet<TEntity> Set();
+
+        IDbSet<T> Set<T>() where T : class, IEntity;
+    }
+}
