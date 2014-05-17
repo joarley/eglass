581c4230fc183c179914c305c3241b928751b6e6
Joarley Santos <joarley.santos@gmail.com>
2014-05-16 21:04:53 -0300

    -

--- eglass-webapp/app/scripts/controllers/home.js
+++ eglass-webapp/app/scripts/controllers/home.js
@@ -1,15 +1,12 @@
 'use strict';
 
 angular.module('eglass-webapp')
-  .controller('homeCtrl', function($scope) {
-    pageSetUp();
+.controller('homeCtrl', function($scope) {
+  pageSetUp();
 
     /*
        * BASIC
        */
-      $('#dt_basic').dataTable({
-        "sPaginationType" : "bootstrap_full"
-      });
 
-      /* END BASIC */
-  });
+       /* END BASIC */
+     });
