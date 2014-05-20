fef4d3669214ede1b221a1fef57f6e3d86dbc368
rudrigodefreitasoliveira <rodrigodefreitasoliveira@gmail.com>
2014-05-20 15:43:23 -0300

    Repositories

---
+++ eglass-serverapp/Rajastech.EGlass.Infrastructure.Data.EntityFramework/Repositories/StoreRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Rajastech.EGlass.Domain.Core;
+
+namespace Rajastech.EGlass.Infrastructure.Data.EntityFramework.Repositories
+{
+    using Rajastech.EGlass.Domain.StoreAgr;
+    using Rajastech.EGlass.Infrastructure.Data.EntityFramework.Core.Repositories;
+    using System;
+
+    public class StoreRepository : Repository<Store, Guid>, IStoreRepository
+    {
+        public StoreRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
