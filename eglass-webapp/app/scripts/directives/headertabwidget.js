6c9caba8cef1e6b73b592a38fa9ab7d256f586ec
Joarley F Santos <joarley.santos@gmail.com>
2014-06-02 16:38:04 -0300

    estruturação do projeto

--- eglass-webapp/app/scripts/directives/headertabwidget.js
+++ eglass-webapp/app/scripts/directives/headertabwidget.js
@@ -2,14 +2,16 @@
 
 angular.module('eglass-webapp')
   .directive('headerTabWidget', function () {
-    return {
-      template: '',
-      restrict: 'A',
-      link: function postLink(scope, element, attrs) {
-        element.children('a').click(function (e) {
+    function link(scope, element) {
+        element.find('li a').click(function (e) {
           e.preventDefault()
           $(this).tab('show')
         });
-      }
+    }
+
+    return {
+      template: '',
+      restrict: 'A',
+      link: link
     };
   });
