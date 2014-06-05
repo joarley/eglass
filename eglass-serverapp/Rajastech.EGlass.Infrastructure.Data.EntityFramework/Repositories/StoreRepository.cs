442788f1a43d7acda316a0d504d338caa9d3429a
Joarley Santos <joarleyferreira@gmail.com>
2014-06-05 20:12:02 -0300

    Estruturação do projeto

--- eglass-serverapp/Rajastech.EGlass.Infrastructure.Data.EntityFramework/Repositories/StoreRepository.cs
+++ eglass-serverapp/Rajastech.EGlass.Infrastructure.Data.EntityFramework/Repositories/StoreRepository.cs
@@ -4,12 +4,15 @@
     using Rajastech.EGlass.Domain.StoreAgr;
     using Rajastech.EGlass.Infrastructure.Data.EntityFramework.Core;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class StoreRepository : Repository<Store, Guid>, IStoreRepository
     {
         public StoreRepository(IUnitOfWork unitOfWork)
             : base(unitOfWork)
         {
+            
         }
     }
 }
