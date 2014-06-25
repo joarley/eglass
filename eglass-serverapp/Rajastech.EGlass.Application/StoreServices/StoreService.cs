0027cd7a551a2f9650d321e1cda9bcb04931e52c
rudrigodefreitasoliveira <rodrigodefreitasoliveira@gmail.com>
2014-06-25 15:07:46 -0300

    Store

---
+++ eglass-serverapp/Rajastech.EGlass.Application/StoreServices/StoreService.cs
@@ -0,0 +1,22 @@
+﻿namespace Rajastech.EGlass.Application.StoreServices
+{
+    using Rajastech.EGlass.Application.DTO.StoreDTOs;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StoreService
+    {
+        public StoreService()
+        {
+            
+        }
+
+        public StoreAbstractDTO Add(StoreAddDTO newStoreDto)
+        {
+            
+        }
+    }
+}
