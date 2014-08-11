d83ff791cd660e21f7f783f26a57726dc6fba4e9
rudrigodefreitasoliveira <rodrigodefreitasoliveira@gmail.com>
2014-08-11 11:37:19 -0300

    ...

---
+++ eglass-serverapp/Eglass.Presentation.WebbMV5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eglass.Presentation.WebbMV5.MvcApplication" Language="C#" %>
---
+++ eglass-serverapp/Eglass.Presentation.WebbMV5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eglass.Presentation.WebbMV5.Startup))]
+namespace Eglass.Presentation.WebbMV5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
